7246d4a79bc7f94c9ebc121cff394151942a2334
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-05-06 20:06:52 -0600

    Completed files 4-9

--- src/04_printing.py
+++ src/04_printing.py
@@ -13,5 +13,6 @@ z = "I like turtles!"
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
-
+f'x is {x}, y is {y}, z is {z}'
 # Finally, print the same thing using an f-string
+'x is {}, y is {}, z is {}'.format(x,y,z)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -15,33 +15,36 @@ has the following keys:
  - name: a name string for this location
 """
 
-waypoints = [
-    {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
-    },
-    {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
-    },
-    {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
-    }
-]
+waypoints = [{
+    "lat": 43,
+    "lon": -121,
+    "name": "a place"
+}, {
+    "lat": 41,
+    "lon": -123,
+    "name": "another place"
+}, {
+    "lat": 43,
+    "lon": -122,
+    "name": "a third place"
+}, {
+    "lat": 111,
+    "lon": 111,
+    "name": "North Pole"
+}]
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
+# print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
 # YOUR CODE HERE
-
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
+print(waypoints)
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+print([v for i in waypoints for k,v in i.items()])
