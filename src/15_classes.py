672142c8303771b3e61977cc00efecf3ae1e05be
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-05-09 14:59:59 -0600

    Added the __str__ method to the Waypoint class
    This allows for easy human readable text when it is printed out using a print() comand. Performed some formatting on the LatLon class

--- src/15_classes.py
+++ src/15_classes.py
@@ -8,8 +8,9 @@ class LatLon:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
-     def __str__(self):
-            return f"{self.lat}, {self.lon}"
+
+    def __str__(self):
+        return f"{self.lat}, {self.lon}"
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
@@ -22,6 +23,8 @@ class Waypoint(LatLon):
         super().__init__(lat, lon)
         self.name = name
 
+    def __str__(self):
+        return f"{self.name}"
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from? The Waypoint class.
